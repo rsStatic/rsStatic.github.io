e9a9d7928fded27338c95d80385c737151621015
rsStatic <info@robinschoors.com>
2013-08-05 00:46:30 +0200

    Update form-generator.js

--- js/form-generator.js
+++ js/form-generator.js
@@ -6,6 +6,7 @@ var row = 0;
 
 function insert_row() {
   $j("tr:last").after('<tr><td><input id="label['+row+']" type="text" /></td><td><select id="input_type['+row+']"><option value="text">Text</option><option value="options">Options</option></select></td><td><input id="ID['+row+']" type="text" /></td><td><input id="value['+row+']" type="text" /></td><td style="text-align: center;"><a onclick="insert_row()" style="font-size:1.5em; margin:0; padding:0">+</a></td></tr>');
+  $j(".total_rows").text(row);
   row++;
 }
 
