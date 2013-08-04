ee4801188ba15741b520ea1e86862e7d8f0699d6
Robin <getmd5@outlook.com>
2013-08-04 18:52:03 +0200

    Form Generator #3.2.1

--- js/form-generator.js
+++ js/form-generator.js
@@ -5,7 +5,7 @@ $j=jQuery.noConflict();
 $j(document).ready(function(){
   alert('ready');
   for(i=1;i<4;i++){
-    alert('Here we go! Looping nr: ' + i);
+    alert('Breeee! Looping nr: ' + i);
     $j("tr:last-of-type").append('<td><input id="label[0]" type="text" /></td><td><select id="input_type[0]"><option value="text">Text</option><option value="options">Options</option></select></td><td><input id="ID[0]" type="text" /></td><td><input id="value[0]" type="text" /></td><td style="text-align: center;">+</td>');
   }
 });
