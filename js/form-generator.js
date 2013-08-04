076178bb88456a3129f358c300d8bee2f581bf5d
rsStatic <info@robinschoors.com>
2013-08-05 00:38:03 +0200

    Update form-generator.js

--- js/form-generator.js
+++ js/form-generator.js
@@ -2,19 +2,14 @@
 
 $j=jQuery.noConflict();
 
-function insert_row(loop) {
-  console.log("Value: " + loop);
-  loop = loop || 1;
-  loop = parseInt(loop);
-  for(i=0;i<loop;i++)
-    console.log("Loop # "+i);
-    $j("tr:last").after('<tr><td><input id="label[0]" type="text" /></td><td><select id="input_type[0]"><option value="text">Text</option><option value="options">Options</option></select></td><td><input id="ID[0]" type="text" /></td><td><input id="value[0]" type="text" /></td><td style="text-align: center;"><a onclick="insert_row()" style="font-size:1.5em; margin:0; padding:0">+</a></td></tr>');
-  }
+function insert_row() {
+  $j("tr:last").after('<tr><td><input id="label[0]" type="text" /></td><td><select id="input_type[0]"><option value="text">Text</option><option value="options">Options</option></select></td><td><input id="ID[0]" type="text" /></td><td><input id="value[0]" type="text" /></td><td style="text-align: center;"><a onclick="insert_row()" style="font-size:1.5em; margin:0; padding:0">+</a></td></tr>');
 }
 
 function init() {
-  console.log("Running init");
-  insert_row(3);
+  for(i=0;i<3;i++) {
+    insert_row();
+  }
 }
 
 $j(document).ready(function(){
