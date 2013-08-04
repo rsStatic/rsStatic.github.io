7216e366cf259a2b582175c03b6713bd1be624f4
rsStatic <info@robinschoors.com>
2013-08-04 19:38:44 +0200

    Update form-generator.js

--- js/form-generator.js
+++ js/form-generator.js
@@ -2,8 +2,11 @@
 
 $j=jQuery.noConflict();
 
-function insert_row() {
-  $j("tr:last").after('<tr><td><input id="label[0]" type="text" /></td><td><select id="input_type[0]"><option value="text">Text</option><option value="options">Options</option></select></td><td><input id="ID[0]" type="text" /></td><td><input id="value[0]" type="text" /></td><td style="text-align: center;"><a onclick="insert_row()" style="font-size:1.5em; margin:0; padding:0">+</a></td></tr>');
+function insert_row(loop) {
+  loop = parseInt(loop) || 1;
+  for(i=0;i<loop;i++)
+    $j("tr:last").after('<tr><td><input id="label[0]" type="text" /></td><td><select id="input_type[0]"><option value="text">Text</option><option value="options">Options</option></select></td><td><input id="ID[0]" type="text" /></td><td><input id="value[0]" type="text" /></td><td style="text-align: center;"><a onclick="insert_row()" style="font-size:1.5em; margin:0; padding:0">+</a></td></tr>');
+  }
 }
 
 $j(document).ready(function(){
