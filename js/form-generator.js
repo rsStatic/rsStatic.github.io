e8b13478e315f6378b2facedffe886f3a779e8c3
Robin <getmd5@outlook.com>
2013-08-04 18:16:12 +0200

    Form Test #1

---
+++ js/form-generator.js
@@ -0,0 +1,10 @@
+/* Form Generator By Robin Schoors */
+
+$j=jQuery.noConflict();
+
+$j(document).ready(function(){
+  alert('ready');
+  for(i=1;i<4;i++){
+    alert('Wheee! Looping nr: ' + i);
+  }
+});
