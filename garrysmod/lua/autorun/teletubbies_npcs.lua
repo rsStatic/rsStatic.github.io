623a180db54bd5010884bdfac2b37cea3c94b97e
Robin <getmd5@outlook.com>
2013-09-12 10:13:00 +0200

    +addon

---
+++ garrysmod/lua/autorun/teletubbies_npcs.lua
@@ -0,0 +1,49 @@
+local Category = "teletubbies_npc_player_v3"
+
+local NPC = { 	Name = "tinkywinky",
+				Class = "npc_citizen",
+				Model = "models/humans/tinkywinky_hl2mp_human/tinkywinky_hl2mp_human.mdl",
+				Squadname = "teletubbies",
+				Numgrenades = "1",
+				Health = "200",
+				KeyValues = { citizentype = 4 },
+				Icon = "materials\vgui\entities\npc_tinkywinky",
+				Category = Category	}
+
+list.Set( "NPC", "npc_tinkywinky", NPC )
+
+local NPC = { 	Name = "dipsy",
+				Class = "npc_citizen",
+				Model = "models/humans/dipsy_hl2mp_human/dipsy_hl2mp_human.mdl",
+				Squadname = "teletubbies",
+				Numgrenades = "1",
+				Health = "200",
+				KeyValues = { citizentype = 4 },
+				Icon = "materials\vgui\entities\npc_dipsy",
+				Category = Category	}
+
+list.Set( "NPC", "npc_dipsy", NPC )
+
+local NPC = { 	Name = "laalaa",
+				Class = "npc_citizen",
+				Model = "models/humans/laalaa_hl2mp_human/laalaa_hl2mp_human.mdl",
+				Squadname = "teletubbies",
+				Numgrenades = "1",
+				Health = "200",
+				KeyValues = { citizentype = 4 },
+				Icon = "materials\vgui\entities\npc_laalaa",
+				Category = Category	}
+
+list.Set( "NPC", "npc_laalaa", NPC )
+
+local NPC = { 	Name = "po",
+				Class = "npc_citizen",
+				Model = "models/humans/po_hl2mp_human/po_hl2mp_human.mdl",
+				Squadname = "teletubbies",
+				Numgrenades = "1",
+				Health = "200",
+				KeyValues = { citizentype = 4 },
+				Icon = "materials\vgui\entities\npc_po",
+				Category = Category	}
+
+list.Set( "NPC", "npc_po", NPC )
---
+++ garrysmod/lua/autorun/teletubbies_playermodels.lua
@@ -0,0 +1,11 @@
+if ( SERVER ) then
+player_manager.AddValidModel("tinkywinky_hl2mp_player","models/player/tinkywinky_hl2mp_player/tinkywinky_hl2mp_player.mdl")
+player_manager.AddValidModel("dipsy_hl2mp_player","models/player/dipsy_hl2mp_player/dipsy_hl2mp_player.mdl")
+player_manager.AddValidModel("laalaa_hl2mp_player","models/player/laalaa_hl2mp_player/laalaa_hl2mp_player.mdl")
+player_manager.AddValidModel("po_hl2mp_player","models/player/po_hl2mp_player/po_hl2mp_player.mdl")
+end
+
+list.Set("PlayerOptionsModel","tinkywinky_hl2mp_player","models/player/tinkywinky_hl2mp_player/tinkywinky_hl2mp_player.mdl")
+list.Set("PlayerOptionsModel","dipsy_hl2mp_player","models/player/dipsy_hl2mp_player/dipsy_hl2mp_player.mdl")
+list.Set("PlayerOptionsModel","laalaa_hl2mp_player","models/player/laalaa_hl2mp_player/laalaa_hl2mp_player.mdl")
+list.Set("PlayerOptionsModel","po_hl2mp_player","models/player/po_hl2mp_player/po_hl2mp_player.mdl")
