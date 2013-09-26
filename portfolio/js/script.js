5976ebbc225e00522d64dcf449b9ad4cca0a2146
Robin <getmd5@outlook.com>
2013-09-26 22:26:29 +0200

    update

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,3 +1,9 @@
-$(function() {
+$(document).ready(function() {
+	var article = $('#section').size();
 
+	$("#container").hide().delay(100).fadeIn();
+
+	$("article").each(function(index){
+		$(this).hide().delay(500).slideDown();
+	});
 });
